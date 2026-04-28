3310365420f3eafae255f0f8a9cdc71701ac21bc
formatecthiago <formatec.thiago@gmail.com>
2026-04-28 08:59:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
-// config.js - COPIE E COLE ISSO EXATAMENTE
+// config.js - CÓDIGO FINAL CORRIGIDO
 const firebaseConfig = {
   apiKey: "AIzaSyDnHzXTN8YgHsJYivJv9yTkc_pnU5E2fME",
-  authDomain: "control-door-f501d.firebaseapp.com",
-  databaseURL: "https://control-door-f501d-default-rtdb.firebaseio.com",
+  authDomain: "://firebaseapp.com",
   projectId: "control-door-f501d",
   storageBucket: "control-door-f501d.firebasestorage.app",
   messagingSenderId: "590100166898",
-  appId: "1:590100166898:web:b234ee092ec3be2fb2786b"
+  appId: "1:590100166898:web:b234ee092ec3be2fb2786b",
+  databaseURL: "https://firebaseio.com"
 };
 
-// Inicializa o Firebase
+// Inicializa o Firebase globalmente
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
+
+// Define a variável db para ser usada em todos os arquivos
 const db = firebase.database();
