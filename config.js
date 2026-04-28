cbfee898c403df7dcef8dbe384ad5ce6f013ae7c
formatecthiago <formatec.thiago@gmail.com>
2026-04-28 04:08:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// config.js - Arquivo de Configuração Central
 const firebaseConfig = {
   apiKey: "AIzaSyDnHzXTN8YgHsJYivJv9yTkc_pnU5E2fME",
   authDomain: "://firebaseapp.com",
@@ -8,9 +7,5 @@ const firebaseConfig = {
   appId: "1:590100166898:web:b234ee092ec3be2fb2786b",
   databaseURL: "https://firebaseio.com"
 };
-
-// Inicializa o Firebase (usando a versão compat que colocamos no HTML)
 firebase.initializeApp(firebaseConfig);
-
-// Cria a conexão com o banco de dados em tempo real
 const db = firebase.database();
