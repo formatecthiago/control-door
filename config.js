e8c3d72d4dc5889b564880eb30c70d688d88ad81
formatecthiago <formatec.thiago@gmail.com>
2026-04-28 17:48:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,21 @@
+// config.js - DEFINITIVO
 const firebaseConfig = {
   apiKey: "AIzaSyDnHzXTN8YgHsJYivJv9yTkc_pnU5E2fME",
   authDomain: "://firebaseapp.com",
   projectId: "control-door-f501d",
-  storageBucket: "control-door-f501d.firebasestorage.app",
+  storageBucket: "://appspot.com",
   messagingSenderId: "590100166898",
   appId: "1:590100166898:web:b234ee092ec3be2fb2786b",
   databaseURL: "https://firebaseio.com"
 };
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+// Inicialização segura
+try {
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+    var db = firebase.database();
+    console.log("Firebase conectado com sucesso!");
+} catch (e) {
+    console.error("Erro na inicialização do config.js:", e);
 }
-const db = firebase.database();
