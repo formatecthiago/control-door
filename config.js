25d29f5e9141fa225b2fa9f7cf7613b4ac3a5e88
formatecthiago <formatec.thiago@gmail.com>
2026-04-28 05:55:40 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-// config.js
+// config.js - COPIE E COLE ISSO EXATAMENTE
 const firebaseConfig = {
   apiKey: "AIzaSyDnHzXTN8YgHsJYivJv9yTkc_pnU5E2fME",
-  authDomain: "://firebaseapp.com",
+  authDomain: "control-door-f501d.firebaseapp.com",
+  databaseURL: "https://control-door-f501d-default-rtdb.firebaseio.com",
   projectId: "control-door-f501d",
   storageBucket: "control-door-f501d.firebasestorage.app",
   messagingSenderId: "590100166898",
-  appId: "1:590100166898:web:b234ee092ec3be2fb2786b",
-  databaseURL: "https://firebaseio.com"
+  appId: "1:590100166898:web:b234ee092ec3be2fb2786b"
 };
 
-// Inicializa o Firebase globalmente
+// Inicializa o Firebase
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
