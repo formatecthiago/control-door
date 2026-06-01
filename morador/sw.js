1efe5149acc95d55fa5a971743e89af8608faa1c
formatecthiago <formatec.thiago@gmail.com>
2026-06-01 01:44:57 -0300

    Refactor service worker caching strategy and assets

--- morador/sw.js
+++ morador/sw.js
@@ -1,38 +1,21 @@
-const CACHE_NAME = 'virty-morador-v2';
-const ASSETS = [
-  '/morador.html',
-  '/manifest.json'
+const CACHE_NAME = 'virty-morador-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
 ];
 
-// Instalação do Service Worker e Cache dos arquivos do Morador
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting())
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Ativação e limpeza de caches antigos
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// Estratégia de Cache: Network First (Tenta buscar a rede atualizada, se falhar usa o cache)
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
