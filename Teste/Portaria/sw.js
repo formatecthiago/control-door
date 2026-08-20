d354d82cfa5b09959e20a9023136b2e5a7c269cc
formatecthiago <formatec.thiago@gmail.com>
2026-08-19 21:40:16 -0300

    Refactor service worker for caching and activation
    Removed ASSETS array and added skipWaiting and claim on activate.

--- Teste/Portaria/sw.js
+++ Teste/Portaria/sw.js
@@ -1,17 +1,15 @@
 const CACHE_NAME = 'portaria-v1';
-const ASSETS = [
-  './index.html',
-  './icon.svg'
-];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
