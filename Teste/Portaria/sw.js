6bbdeec80fedac2991cbaae49730d460715164bb
formatecthiago <formatec.thiago@gmail.com>
2026-08-19 21:20:19 -0300

    Add caching logic to service worker
    Implement service worker caching for PWA assets.

--- Teste/Portaria/sw.js
+++ Teste/Portaria/sw.js
@@ -1,6 +1,17 @@
+const CACHE_NAME = 'portaria-v1';
+const ASSETS = [
+  './index.html',
+  './icon.svg'
+];
 
-//sw.js - Arquivo Service Worker básico
-self.addEventListener('fetch', function(event) {
-// Apenas uma estrutura para o navegador aceitar como PWA
-event.respondWith(fetch(event.request));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
